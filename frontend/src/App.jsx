f19978e3eb7cdad66eb283030402c9c93bab80a0
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-12-08 12:19:08 +0100

    installation de react-router-dom, tailwind, hashlink, toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
