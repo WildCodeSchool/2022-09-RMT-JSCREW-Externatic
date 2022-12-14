2171e9ecec84ec0ff41760075bba188a871d8cf4
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-12-14 10:08:29 +0100

    ajout des couleurs dans la config de tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    colors: {
+      black: "#404040",
+      white: "#FFFFFF",
+      pink: "#C91961",
+      darkPink: "#851342",
+    },
   },
   plugins: [],
-}
+};
