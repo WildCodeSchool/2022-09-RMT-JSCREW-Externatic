3f9418499740a1bafc2760eb5ed1e2a054e1b51d
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-12-16 10:02:56 +0100

    ajout de la nouvelle image principale et continuation du responsive de la page home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,11 +24,11 @@ export default function Home() {
           S'inscrire
         </button>
       </div>
-      <div className="w-5/6 mx-auto flex flex-col items-center">
-        <h1 className="text-xl font-bold">
+      <div className="w-5/6 mx-auto  mt-4 flex flex-col items-center">
+        <h1 className="text-xl font-bold md:text-3xl">
           Externatic est une plateforme créée pour les métiers de l'informatique
         </h1>
-        <p>
+        <p className="md:text-2xl">
           ici, chaque visage a un nom. Nos consultants ne sont pas des robots,
           il n'y a pas d'algorithmes, de profils ou de liens automatisés.{" "}
         </p>
@@ -44,43 +44,45 @@ export default function Home() {
         <h2 className="text-center text-white font-bold pt-4 text-2xl">
           La réussite d’un recrutement Externatic repose sur
         </h2>
-        <div className="mt-5 w-11/12 mx-auto">
-          <div className="flex space-x-16 items-center">
-            <img alt="icone mains" src={icon1} className="w-12" />
-            <h3 className="text-white font-bold ">Notre proximité</h3>
+        <div className="flex flex-col md:flex-row">
+          <div className="mt-5 w-11/12  md:w-96 mx-auto">
+            <div className="flex space-x-16 items-center">
+              <img alt="icone mains" src={icon1} className="w-12" />
+              <h3 className="text-white font-bold ">Notre proximité</h3>
+            </div>
+            <p className="text-white md:mt-2">
+              L’expérience professionnelle est une chose. L’expérience de vie en
+              est une autre. Alors nos consultants prennent le temps de faire
+              connaissance avec chaque personne, pour comprendre le contexte, le
+              parcours, les envies et les projets.
+            </p>
           </div>
-          <p className="text-white">
-            L’expérience professionnelle est une chose. L’expérience de vie en
-            est une autre. Alors nos consultants prennent le temps de faire
-            connaissance avec chaque personne, pour comprendre le contexte, le
-            parcours, les envies et les projets.
-          </p>
-        </div>
-        <div className="mt-5 w-11/12 mx-auto">
-          <div className="flex space-x-16 items-center">
-            <img alt="icone fusée" src={icon2} className="w-12" />
-            <h3 className="text-white font-bold">Notre performance</h3>
+          <div className="mt-5 w-11/12 md:w-96 mx-auto">
+            <div className="flex space-x-16 items-center">
+              <img alt="icone fusée" src={icon2} className="w-12" />
+              <h3 className="text-white font-bold">Notre performance</h3>
+            </div>
+            <p className="text-white md:mt-2">
+              Notre réseau est une force et nous y travaillons sans relâche.
+              Notre expérience nous permet d’identifier les vrais besoins d’une
+              entreprise et de ceux qui la rejoignent.
+            </p>
           </div>
-          <p className="text-white">
-            Notre réseau est une force et nous y travaillons sans relâche. Notre
-            expérience nous permet d’identifier les vrais besoins d’une
-            entreprise et de ceux qui la rejoignent.
-          </p>
-        </div>
-        <div className="mt-5 w-11/12 mx-auto">
-          <div className="flex space-x-16 items-center">
-            <img alt="icone sablier" src={icon3} className="w-12" />
-            <h3 className="text-white font-bold text-center">
-              Notre durabilité
-            </h3>
+          <div className="mt-5 w-11/12 md:w-96 mx-auto">
+            <div className="flex space-x-16 items-center">
+              <img alt="icone sablier" src={icon3} className="w-12" />
+              <h3 className="text-white font-bold text-center">
+                Notre durabilité
+              </h3>
+            </div>
+            <p className="text-white pb-4 md:mt-2">
+              Notre challenge est de trouver l’équipe qui fonctionnera ensemble
+              de manière professionnelle et personnelle, pour aller jusqu’au
+              bout d’un projet commun. Notre responsabilité vis-à-vis des
+              impacts de nos décisions et nos actions sur le long-terme
+              correspondent également à notre politique RSE.
+            </p>
           </div>
-          <p className="text-white pb-4">
-            Notre challenge est de trouver l’équipe qui fonctionnera ensemble de
-            manière professionnelle et personnelle, pour aller jusqu’au bout
-            d’un projet commun. Notre responsabilité vis-à-vis des impacts de
-            nos décisions et nos actions sur le long-terme correspondent
-            également à notre politique RSE.
-          </p>
         </div>
         <iframe
           className="w-full aspect-video ..."
