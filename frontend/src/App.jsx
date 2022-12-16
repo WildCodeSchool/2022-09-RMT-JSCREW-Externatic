22d3e000694f415b7dc24a1f352b005758be6f5c
serpentaire <glemoine@hotmail.fr>
2022-12-16 08:49:41 +0100

    création de la nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Router>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
