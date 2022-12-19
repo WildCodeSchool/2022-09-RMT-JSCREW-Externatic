039f4a867405fe54a2c314ab502a045cbefb549e
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-12-19 18:06:53 +0100

    ajout fix Offremanager.js

--- backend/src/models/OffreManager.js
+++ backend/src/models/OffreManager.js
@@ -1,4 +1,3 @@
-const { rand } = require("../controllers/offreControllers");
 const AbstractManager = require("./AbstractManager");
 
 class OffreManager extends AbstractManager {
@@ -6,22 +5,11 @@ class OffreManager extends AbstractManager {
     super({ table: "offre" });
   }
 
-  insert(offre) {
-    return this.connection.query(
-      `insert into ${this.table} (title) values (?)`,
-      [offre.title]
-    );
-  }
-
-  update(offre) {
-    return this.connection.query(
-      `update ${this.table} set title = ? where id = ?`,
-      [offre.title, offre.id]
-    );
-  }
-
   rand(number) {
-    return this.connection.query(`select poste, localisation from  ${this.table} ORDER BY rand() LIMIT ?`, [number]);
+    return this.connection.query(
+      `select poste, localisation from  ${this.table} ORDER BY rand() LIMIT ?`,
+      [number]
+    );
   }
 }
 
