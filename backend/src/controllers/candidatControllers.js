8c0b6fc10cd76a551a61983474ffd83a923d8a76
serpentaire <glemoine@hotmail.fr>
2022-12-20 14:45:08 +0100

    add JOI

--- backend/src/controllers/candidatControllers.js
+++ backend/src/controllers/candidatControllers.js
@@ -1,19 +1,22 @@
 const models = require("../models");
+const validate = require("../service/candidat");
 
 const add = (req, res) => {
   const candidat = req.body;
-
-  // TODO validations (length, format...)
-
-  models.candidat
-    .insert(candidat)
-    .then(([result]) => {
-      res.location(`/profil/${result.insertId}`).sendStatus(201);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
+  const error = validate(candidat);
+  if (error) {
+    res.status(422).send(error);
+  } else {
+    models.candidat
+      .insert(candidat)
+      .then(([result]) => {
+        res.location(`/profil/${result.insertId}`).sendStatus(201);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  }
 };
 
 module.exports = {
