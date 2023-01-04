fc1462016551ee562ee3ef7565cef2bebe191d85
serpentaire <glemoine@hotmail.fr>
2023-01-04 16:11:31 +0100

    add upload files in backend

--- backend/src/controllers/candidatControllers.js
+++ backend/src/controllers/candidatControllers.js
@@ -3,20 +3,23 @@ const validate = require("../service/candidat");
 
 const add = (req, res) => {
   const candidat = req.body;
-  const error = validate(candidat);
-  if (error) {
-    res.status(422).send(error);
-  } else {
-    models.candidat
-      .insert(candidat)
-      .then(([result]) => {
-        res.location(`/profil/${result.insertId}`).sendStatus(201);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
-  }
+  console.log(req.files);
+  console.log(candidat);
+  res.sendStatus(201)
+  // const error = validate(candidat);
+  // if (error) {
+  //   res.status(422).send(error);
+  // } else {
+  //   models.candidat
+  //     .insert(candidat)
+  //     .then(([result]) => {
+  //       res.location(`/profil/${result.insertId}`).sendStatus(201);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //       res.sendStatus(500);
+  //     });
+  // }
 };
 
 module.exports = {
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,17 +1,33 @@
 const express = require("express");
+const multer = require("multer");
 
 const router = express.Router();
 const offreControllers = require("./controllers/offreControllers");
 const entrepriseControllers = require("./controllers/entrepriseControllers");
 const candidatControllers = require("./controllers/candidatControllers");
 
+//configuration de l'upload profil
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets')
+  },
+  filename: function (req, file, cb) {
+    const fileArray = file.originalname.split(".");
+    const ext = fileArray.pop();
+    const filename = fileArray.join("_").split(" ").join("_");
+    cb(null, `${file.fieldname === "cv" ? "/cv/" : "/avatar/"}${filename + "_" + Date.now() + "." + ext}`)
+  }
+})
+
+const upload = multer({ storage: storage });
+//fin de la configuration de l'upload
+
 router.get("/entreprises", entrepriseControllers.browse);
 router.post("/entreprises", entrepriseControllers.add);
 
-router.post("/profil", candidatControllers.add);
+router.post("/profil",upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cv', maxCount: 1 }]) ,candidatControllers.add);
 router.get("/offres", offreControllers.browse);
 router.get("/offres/rand", offreControllers.random);
 router.post("/offres", offreControllers.add);
-
 
 module.exports = router;
