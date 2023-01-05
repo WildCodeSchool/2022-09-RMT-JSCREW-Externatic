caa55265727745405009d681775f57ae181ddda9
serpentaire <glemoine@hotmail.fr>
2023-01-05 21:58:36 +0100

    added option files photo or cv selected or not

--- backend/src/controllers/candidatControllers.js
+++ backend/src/controllers/candidatControllers.js
@@ -3,8 +3,18 @@ const validate = require("../service/candidat");
 
 const add = (req, res) => {
   let candidat = req.body.data;
-  const profilPhoto = `public/assets${req.files.avatar[0].filename}`;
-  const profilCv = `public/assets${req.files.cv[0].filename}`;
+  let profilPhoto = "";
+  let profilCv = "";
+  if (req.files.avatar !== undefined) {
+    profilPhoto = `public/assets${req.files.avatar[0].filename}`;
+  } else {
+    profilPhoto = `public/assets/avatar/Avatar.png`;
+  }
+  if (req.files.cv !== undefined) {
+    profilCv = `public/assets${req.files.cv[0].filename}`;
+  } else {
+    profilCv = `public/assets/cv/cv.png`;
+  }
   candidat = JSON.parse(candidat);
   const error = validate(candidat);
   if (error) {
