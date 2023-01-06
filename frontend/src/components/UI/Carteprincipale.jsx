8fc156179c7d4f170f5a679d8e82d5426605ea39
Alexandre Rouxel <104839076+arouxel17@users.noreply.github.com>
2023-01-06 12:33:02 +0100

    Update Carteprincipale.jsx

--- frontend/src/components/UI/Carteprincipale.jsx
+++ frontend/src/components/UI/Carteprincipale.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 function CartePrincipale({ id }) {
-  const [offre, setOffre] = useState();
+  const [principale, setPrincipale] = useState();
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/offres/${id}`)
       .then((response) => response.json())
       .then((data) => {
-        setOffre(data);
+        setPrincipale(data);
       })
       .catch((err) => console.error(err));
   }, []);
@@ -15,25 +15,25 @@ function CartePrincipale({ id }) {
   return (
     <div className="flex flex-col min-w-full justify-items-center items-center my-5 bg-darkPink text-white rounded-3xl drop-shadow-xl">
       <h1 className="text-center text-2xl underline underline-offset-8 my-4">
-        {offre?.poste}
+        {principale?.poste}
       </h1>
       <div className="flex flex-row justify-center">
         <h2 className="flex items-center justify-center w-32 lg:w-48 xl:w-64 bg-pink rounded-2xl border-2 text-center text-lg mx-4 my-4">
-          {offre?.contrat}
+          {principale?.contrat}
         </h2>
         <h2 className="flex items-center justify-center w-32 lg:w-48 xl:w-64 bg-pink rounded-2xl border-2 text-center text-lg mx-4 my-4">
-          {offre?.condition_travail}
+          {principale?.condition_travail}
         </h2>
       </div>
       <h2 className="w-90 md:w-96 p-1 bg-white rounded-2xl text-darkPink text-center text-lg mx-4 my-4">
-        {offre?.specialitees}
+        {principale?.specialitees}
       </h2>
       <div className="flex flex-row justify-center">
         <h2 className="flex items-center justify-center w-32 lg:w-48 xl:w-64 bg-pink rounded-2xl border-2 text-center text-lg mx-4 my-4">
-          Posté le : {offre?.dateOffre}
+          Posté le : {principale?.dateOffre}
         </h2>
         <h2 className="flex items-center justify-center w-32 lg:w-48 xl:w-64 bg-pink rounded-2xl border-2 text-center text-lg mx-4 my-4">
-          Fin des candidatures : {offre?.date_fin_offre}
+          Fin des candidatures : {principale?.date_fin_offre}
         </h2>
       </div>
       <div>
@@ -41,7 +41,7 @@ function CartePrincipale({ id }) {
           Missions
         </h1>
         <p className="w-90 md:w-96 text-justify my-4 mx-6">
-          🎯 {offre?.mission}
+          🎯 {principale?.mission}
         </p>
       </div>
     </div>
