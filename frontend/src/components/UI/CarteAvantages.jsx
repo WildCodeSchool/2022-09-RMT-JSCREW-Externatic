28941820ca7e1b8985926d77d86f1454b8c46b08
Alexandre Rouxel <104839076+arouxel17@users.noreply.github.com>
2023-01-10 17:20:09 +0100

    Update CarteAvantages.jsx

--- frontend/src/components/UI/CarteAvantages.jsx
+++ frontend/src/components/UI/CarteAvantages.jsx
@@ -1,17 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-function CarteAvantages({ id }) {
-  const [avantages, setAvantages] = useState();
-
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/offres/${id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setAvantages(data);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-
+function CarteAvantages({ offre }) {
   return (
     <div className="flex flex-col min-w-full justify-items-center items-center p-4 mx-6 my-5 text-darkPink text-justify border-2 rounded-3xl drop-shadow-xl">
       <h1 className="underline underline-offset-8 mx-4 mb-4">Avantages 🧨</h1>
