edbea79c535685e27a27f673eca5f04799e823c5
Alexandre Rouxel <104839076+arouxel17@users.noreply.github.com>
2023-01-10 17:16:53 +0100

    Update UneOffre.jsx

--- frontend/src/pages/UneOffre/UneOffre.jsx
+++ frontend/src/pages/UneOffre/UneOffre.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import CartePrincipale from "@components/UI/Carteprincipale";
 import CarteEntreprise from "@components/UI/CarteEntreprise";
@@ -9,6 +9,16 @@ import offreentete from "@assets/offre_emploi.jpg";
 
 function UneOffre() {
   const { id } = useParams();
+  const [offre, setOffre] = useState();
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/offres/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setOffre(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div className="container-offre">
@@ -17,12 +27,12 @@ function UneOffre() {
       </div>
       <div className="flex flex-col md:flex-row justify-center items-center">
         <div className="flex justify-center md:w-6/12 lg:w-6/12 xl:w-6/12 mx-6">
-          <CartePrincipale id={id} />
+          <CartePrincipale offre={offre} />
         </div>
         <div className="flex flex-col md:w-6/12 lg:w-4/12 xl:w-4/12 mx-6 items-center">
-          <CarteEntreprise id={id} />
-          <CarteProfil id={id} />
-          <CarteSalaire id={id} />
+          <CarteEntreprise offre={offre} />
+          <CarteProfil offre={offre} />
+          <CarteSalaire offre={offre} />
         </div>
       </div>
       <ListOfOffers />
