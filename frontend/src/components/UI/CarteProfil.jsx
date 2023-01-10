ee10a5878b5ea653950caed152000ff976f3f970
Alexandre Rouxel <104839076+arouxel17@users.noreply.github.com>
2023-01-10 17:19:20 +0100

    Update CarteProfil.jsx

--- frontend/src/components/UI/CarteProfil.jsx
+++ frontend/src/components/UI/CarteProfil.jsx
@@ -1,17 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-function CarteProfil({ id }) {
-  const [profil, setProfil] = useState();
-
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/offres/${id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setProfil(data);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-
+function CarteProfil({ offre }) {
   return (
     <div className="flex flex-col min-w-full justify-items-center items-center p-4 mx-6 my-5 text-darkPink text-justify border-2 rounded-3xl drop-shadow-xl">
       <h1 className="underline underline-offset-8 mx-4 mb-4">
