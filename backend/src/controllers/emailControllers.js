9d8eac1a1b299ef71e545aefe0100bd0ebbf3d5e
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2023-01-31 14:39:09 +0100

    retrait code commenté

--- backend/src/controllers/emailControllers.js
+++ backend/src/controllers/emailControllers.js
@@ -1,16 +1,7 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const sendMail = async (email) => {
-  // const email = {
-  //   name: req.nom,
-  //   surname: req.prenom,
-  //   phone: req.telephone,
-  //   email: req.email,
-  //   message:
-  //     "Votre inscription a bien été prise en compte. Un consultant prendra bientôt contact avec vous.",
-  // };
-
+const sendMail = async (mailOptions) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_SENDIN,
     port: process.env.SMTP_PORT_SENDIN,
@@ -20,21 +11,6 @@ const sendMail = async (email) => {
       pass: process.env.SMTP_SENDIN_PASSWORD,
     },
   });
-
-  const mailOptions = {
-    from: "glemoine@hotmail.fr",
-    to: email.email, // this is the address to which the email will be sent
-    subject: "Nouveau message d'Externatic",
-    attachments: [
-      {
-        filename: "Logo-Externatic.png",
-        path: "public/assets/images/Logo-Externatic.png",
-        cid: "logo",
-      },
-    ],
-    text: `${email.message} \n\n Phone: ${email.phone} \n\n Name: ${email.name} \n\n Surname: ${email.surname} \n\n Email: ${email.email}`,
-    html: `<p>Bonjour,</p> <p>${email.message}</p> <p>Rappel de vos coordonnées :</p><p>Téléphone: ${email.phone}</p><p>Nom: ${email.name}</p> <p>Prenom: ${email.surname}</p><p>Email: ${email.email}</p> <img src="cid:logo" height="100" />`,
-  };
 
   return transporter
     .sendMail(mailOptions)
